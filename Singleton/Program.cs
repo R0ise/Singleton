e0892a234d352aa4d8259dbcef792c223b47e2cf
R0ise <the.roise.511@gmail.com>
2022-01-12 16:42:34 +0400

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Administrator
+    {
+        static Administrator administrator;
+        protected Administrator()
+        {
+            Console.WriteLine("Администратор зашёл на сайт ");
+        }
+        public static Administrator Instance()
+        {
+            if (administrator == null)
+                administrator = new Administrator();
+            else
+                Console.WriteLine("Администратор уже в сети ");
+            return administrator;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Administrator a = Administrator.Instance();
+            Administrator b = Administrator.Instance();
+        }
+    }
+}
